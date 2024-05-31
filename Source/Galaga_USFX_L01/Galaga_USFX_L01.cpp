5ed78e759fe43e16e66260740ddbd82b948211c4
Daniel Aguirre <aguirredanny66@gmail.com>
2024-05-31 06:44:52 -0400

    Revert "Añadir configuración de finales de línea en .gitattributes"
    This reverts commit fce596c818f847d9c2f5bc5a86fa32fddaf2478a.

--- Source/Galaga_USFX_L01/Galaga_USFX_L01.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Galaga_USFX_L01.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Galaga_USFX_L01, "Galaga_USFX_L01" );
-
-DEFINE_LOG_CATEGORY(LogGalaga_USFX_L01)
- 
--- Source/Galaga_USFX_L01/Galaga_USFX_L01.h
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#pragma once
-
-#include "CoreMinimal.h"
-
-DECLARE_LOG_CATEGORY_EXTERN(LogGalaga_USFX_L01, Log, All);
